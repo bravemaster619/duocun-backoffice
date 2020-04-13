fd13c7f0fe7a1a7a295c5c688a3d26231a3657ac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-13 17:27:36 +0000

    Deploy bravemaster619/duocun-backoffice to github.com/bravemaster619/duocun-backoffice.git:gh-pages

---
+++ precache-manifest.549156bad82937468ab215adde623c17.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "061b854da3dd58c6a8529cb01a0ab3ce",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1e2d4854c5b8a081df6",
+    "url": "/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "5ffd26e718a6cbef182a",
+    "url": "/static/css/main.1a5f11bd.chunk.css"
+  },
+  {
+    "revision": "b1e2d4854c5b8a081df6",
+    "url": "/static/js/2.aa09440b.chunk.js"
+  },
+  {
+    "revision": "5ffd26e718a6cbef182a",
+    "url": "/static/js/main.74048259.chunk.js"
+  },
+  {
+    "revision": "a7453b08e83a2f93a6c6",
+    "url": "/static/js/runtime~main.6cea818d.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
