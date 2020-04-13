be8435fb4f9b8af14c0370978e5244aeb3e1ffa0
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-13 17:40:32 +0000

    Deploy bravemaster619/duocun-backoffice to github.com/bravemaster619/duocun-backoffice.git:gh-pages

---
+++ precache-manifest.7f4ac6c253fd37406b605996271d8047.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5b28e232b6fedb6be29df2f90d2913e",
+    "url": "/duocun-backoffice/index.html"
+  },
+  {
+    "revision": "b1e2d4854c5b8a081df6",
+    "url": "/duocun-backoffice/static/css/2.d0176e96.chunk.css"
+  },
+  {
+    "revision": "af1a22c84cbc7ff8cf90",
+    "url": "/duocun-backoffice/static/css/main.1a5f11bd.chunk.css"
+  },
+  {
+    "revision": "b1e2d4854c5b8a081df6",
+    "url": "/duocun-backoffice/static/js/2.aa09440b.chunk.js"
+  },
+  {
+    "revision": "af1a22c84cbc7ff8cf90",
+    "url": "/duocun-backoffice/static/js/main.059de936.chunk.js"
+  },
+  {
+    "revision": "633108f645c6ad6c337f",
+    "url": "/duocun-backoffice/static/js/runtime~main.98f18587.js"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/duocun-backoffice/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/duocun-backoffice/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.549156bad82937468ab215adde623c17.js"
+  "/duocun-backoffice/precache-manifest.7f4ac6c253fd37406b605996271d8047.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/duocun-backoffice/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
