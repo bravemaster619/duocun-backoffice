b8bc69a671745a1a19b1116805f9146163f599c3
bravemaster619 <bravemaster619@hotmail.com>
2020-04-14 14:00:36 +0800

    feat(page): add products page

---
+++ src/views/Products/Products.js
@@ -0,0 +1,80 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import { useTranslation } from "react-i18next";
+
+import GridContainer from "components/Grid/GridContainer.js";
+import GridItem from "components/Grid/GridItem.js";
+import Card from "components/Card/Card.js";
+import CardHeader from "components/Card/CardHeader.js";
+import CardBody from "components/Card/CardBody.js";
+import CardFooter from "components/Card/CardFooter.js";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableContainer from "@material-ui/core/TableContainer";
+import TableHead from "@material-ui/core/TableHead";
+import TablePagination from "@material-ui/core/TablePagination";
+import TableRow from "@material-ui/core/TableRow";
+import EditIcon from "@material-ui/icons/Edit";
+
+const useStyles = makeStyles(() => ({
+  table: {
+    minWidth: 750
+  }
+}));
+
+export default function Product() {
+  const { t } = useTranslation();
+
+  const classes = useStyles();
+
+  return (
+    <div>
+      <GridContainer>
+        <GridItem xs={12}>
+          <Card>
+            <CardHeader color="primary">
+              <h4>{t("Product Table")}</h4>
+            </CardHeader>
+            <CardBody>
+              <GridContainer>
+                <GridItem xs={12}>
+                  <TableContainer>
+                    <Table
+                      className={classes.table}
+                      aria-label="Product Table"
+                      size="medium"
+                    >
+                      <TableHead>
+                        <TableRow>
+                          <TableCell>ID</TableCell>
+                          <TableCell>Image</TableCell>
+                          <TableCell>Name</TableCell>
+                          <TableCell>Price</TableCell>
+                          <TableCell>Featured</TableCell>
+                          <TableCell>Actions</TableCell>
+                        </TableRow>
+                      </TableHead>
+                      <TableBody>
+                        <TableRow>
+                          <TableCell>1</TableCell>
+                          <TableCell></TableCell>
+                          <TableCell>Sample Product</TableCell>
+                          <TableCell>9.99 USD</TableCell>
+                          <TableCell>O</TableCell>
+                          <TableCell>
+                            <EditIcon></EditIcon>
+                          </TableCell>
+                        </TableRow>
+                      </TableBody>
+                    </Table>
+                  </TableContainer>
+                </GridItem>
+              </GridContainer>
+            </CardBody>
+          </Card>
+        </GridItem>
+      </GridContainer>
+    </div>
+  );
+}
