2ce04e2a47ac6d8d139f790c76ea1f69b627ec96
bravemaster619 <bravemaster619@hotmail.com>
2020-04-18 20:25:05 +0800

    feat(page): add category page and implement crud operations

---
+++ src/services/api/ApiCategoryService.js
@@ -0,0 +1,34 @@
+import ApiService from "services/api/ApiService";
+import { buildPaginationQuery } from "helper";
+
+export default {
+  getCategoryList: (page, pageSize, parentId = "", sort = []) => {
+    const query = {};
+    if (parentId) {
+      query.query = buildPaginationQuery(
+        page,
+        pageSize,
+        {
+          parentId
+        },
+        [],
+        sort
+      );
+    } else {
+      query.query = buildPaginationQuery(page, pageSize, {}, [], sort);
+    }
+    return ApiService.v2().get("admin/categories", query);
+  },
+  saveCategory: model => {
+    if (!model.id) {
+      model._id ? (model.id = model._id) : (model.id = "new");
+    }
+    return ApiService.v2().post(`admin/categories/${model.id}`, model);
+  },
+  getCategory: id => {
+    return ApiService.v2().get(`admin/categories/${id}`);
+  },
+  removeCategory: id => {
+    return ApiService.v2().delete(`admin/categories/${id}`);
+  }
+};
