cb508fb63769dc2ac625ca9081e1fac85bf10886
bravemaster619 <bravemaster619@hotmail.com>
2020-04-18 20:31:37 +0800

    fix(ci): remove unused import

--- src/views/Categories/ListCategories.js
+++ src/views/Categories/ListCategories.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import PropTypes from "prop-types";
 import { useTranslation } from "react-i18next";
 import { makeStyles } from "@material-ui/core/styles";
